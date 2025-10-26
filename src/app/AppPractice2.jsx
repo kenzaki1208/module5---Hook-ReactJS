f39a24f151d865f212bdfbcf7b1a238f2f49092a
kenzaki1208 <duytrinh25072001@gmail.com>
2025-10-26 14:15:39 +0700

    update project

---
+++ src/app/AppPractice2.jsx
@@ -0,0 +1,12 @@
+import Selector from "../components/selector";
+
+function AppPractice2() {
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>UseEffect Demo</h1>
+            <Selector />
+        </div>
+    );
+}
+
+export default AppPractice2;
